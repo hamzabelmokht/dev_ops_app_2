2026bd85670d16a8a3d3d35bf0008a9679b26098
hamzabelmo <n01649543@humber.ca>
2024-04-07 04:30:15 -0400

    implemented solution

--- app.py
+++ app.py
@@ -25,5 +25,5 @@ def add():
         cur.close()
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+#if __name__ == '__main__':
+ #   app.run(host='0.0.0.0', port=3000, debug=True)
